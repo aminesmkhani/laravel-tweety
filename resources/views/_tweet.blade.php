296e3b4d873c298e42318d9626cd52f709d2b260
Amin Esmkhani <amin.esmkhani75@gmail.com>
2022-02-23 14:02:02 +0330

    add user relation tweet & user!

--- resources/views/_tweet.blade.php
+++ resources/views/_tweet.blade.php
@@ -4,12 +4,10 @@
     </div>
 
     <div>
-        <h5 class="font-semibold mb-4">Amin Esmkhani</h5>
+        <h5 class="font-semibold mb-4">{{$tweet->user->name}}</h5>
 
         <p class="text-sm">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            {{$tweet->body}}
         </p>
     </div>
 </div>
