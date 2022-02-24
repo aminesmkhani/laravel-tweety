ceaac41fe6c399788f337ea5cbd1b7e8a581252d
Amin Esmkhani <amin.esmkhani75@gmail.com>
2022-02-24 08:47:16 +0330

    add need att to tweet form!

--- resources/views/_publish-tweet-panel.blade.php
+++ resources/views/_publish-tweet-panel.blade.php
@@ -1,9 +1,10 @@
 <div class="border border-blue-400 rounded-lg p-8 py-6 mb-8">
-    <form action="#">
-        <textarea name="body" class="w-full" placeholder="what's up doc?"></textarea>
+    <form method="post" action="/tweets">
+        @csrf
+        <textarea name="body" class="w-full" placeholder="what's up doc?" required></textarea>
         <hr class="my-4">
         <footer class="flex justify-between">
-            <img src="{{auth()->user()->avatar}}" alt="" class="rounded-full mr-2">
+            <img src="{{auth()->user()->avatar}}" alt="your avatar" class="rounded-full mr-2">
             <button type="submit" class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white">Tweet-a-roo!</button>
         </footer>
     </form>
