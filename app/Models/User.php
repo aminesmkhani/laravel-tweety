94f304674478a0802c47af0c579b60474f2677a1
Amin Esmkhani <amin.esmkhani75@gmail.com>
2022-02-24 08:44:14 +0330

    Refactor hardcode avatar method & define user model!

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,6 +43,12 @@ class User extends Authenticatable
     ];
 
 
+    public function getAvatarAttribute()
+    {
+        return "https://i.pravatar.cc/40?u=". $this->email;
+    }
+
+
     public function timeline()
     {
         return Tweet::where('user_id',$this->id)->latest()->get();
--- resources/views/_publish-tweet-panel.blade.php
+++ resources/views/_publish-tweet-panel.blade.php
@@ -3,7 +3,7 @@
         <textarea name="body" class="w-full" placeholder="what's up doc?"></textarea>
         <hr class="my-4">
         <footer class="flex justify-between">
-            <img src="https://i.pravatar.cc/40" alt="" class="rounded-full mr-2">
+            <img src="{{auth()->user()->avatar}}" alt="" class="rounded-full mr-2">
             <button type="submit" class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white">Tweet-a-roo!</button>
         </footer>
     </form>
