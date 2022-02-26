efcfa4b19720b60d6705695efa26c2be8e447283
Amin Esmkhani <amin.esmkhani75@gmail.com>
2022-02-26 14:23:42 +0330

    personalize message & new users!

--- resources/views/_friends-list.blade.php
+++ resources/views/_friends-list.blade.php
@@ -1,7 +1,7 @@
 <h3 class="font-semibold text-xl mb-4">Following</h3>
 
 <ul>
-    @foreach(auth()->user()->follows as $user)
+    @forelse(auth()->user()->follows as $user)
     <li class="mb-4">
         <div>
             <a href="{{route('profile',$user)}}" class="flex items-center text-sm">
@@ -10,5 +10,7 @@
             </a>
         </div>
     </li>
-    @endforeach
+    @empty
+        <p>No Friends Yet!</p>
+    @endforelse
 </ul>
