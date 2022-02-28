1816456508c6ac9d511e02ccb57038007291673e
Amin Esmkhani <amin.esmkhani75@gmail.com>
2022-02-28 07:58:40 +0330

    refactor links!

--- resources/views/_sidebar-links.blade.php
+++ resources/views/_sidebar-links.blade.php
@@ -1,10 +1,6 @@
 <ul>
     <li><a href="{{route('home')}}" class="font-semibold text-lg mb-4 block">Home</a></li>
     <li><a href="/explore" class="font-semibold text-lg mb-4 block">Explore</a></li>
-    <li><a href="#" class="font-semibold text-lg mb-4 block">Notifications</a></li>
-    <li><a href="#" class="font-semibold text-lg mb-4 block">Messages</a></li>
-    <li><a href="#" class="font-semibold text-lg mb-4 block">Bookmarks</a></li>
-    <li><a href="#" class="font-semibold text-lg mb-4 block">Lists</a></li>
     <li><a href="{{route('profile',auth()->user())}}" class="font-semibold text-lg mb-4 block">Profile</a></li>
     <li>
         <form method="POST" action="/logout">
